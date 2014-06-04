e029c4ea37f5b27b70ac7bd1d4c398758d3fe625
Bart Voet <bart_voet@telenet.be>
2014-06-04 23:30:57 +0200

    Cleanup of SensorType-interface

--- sensors/sensorapi/src/main/java/org/b/v/sensors/api/SensorType.java
+++ sensors/sensorapi/src/main/java/org/b/v/sensors/api/SensorType.java
@@ -3,7 +3,6 @@ package org.b.v.sensors.api;
 import java.util.List;
 
 public interface SensorType {
-	List<String> getValueTypes();
 	List<String> getConfigurationParameters();
 	List<String> getTypeNames();
 }
--- sensors/sensorapi/src/main/java/org/b/v/sensors/api/support/DefaultSensorType.java
+++ sensors/sensorapi/src/main/java/org/b/v/sensors/api/support/DefaultSensorType.java
@@ -7,25 +7,15 @@ import org.b.v.sensors.api.SensorType;
 
 public class DefaultSensorType implements SensorType {
 
-	private List<String> valueTypes=new ArrayList<String>();
 	private List<String> configurationParameters=new ArrayList<String>();
 	private List<String> typeNames=new ArrayList<String>();
 	
-	public List<String> getValueTypes() {
-		return valueTypes;
-	}
-
 	public List<String> getConfigurationParameters() {
 		return configurationParameters;
 	}
 
 	public List<String> getTypeNames() {
 		return typeNames;
-	}
-	
-	public DefaultSensorType addValueType(String value){
-		valueTypes.add(value);
-		return this;
 	}
 	
 	public DefaultSensorType addConfigurationParameter(String parameter) {
@@ -37,7 +27,5 @@ public class DefaultSensorType implements SensorType {
 		typeNames.add(typeName);
 		return this;
 	}
-	
-	
 
 }

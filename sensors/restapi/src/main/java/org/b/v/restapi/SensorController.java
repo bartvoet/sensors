f4eaa51fa6ba00e82dd78727f81140f097f9cf58
Bart Voet <bart_voet@telenet.be>
2014-06-06 23:14:19 +0200

    Removal of comment

--- sensors/restapi/src/main/java/org/b/v/restapi/SensorController.java
+++ sensors/restapi/src/main/java/org/b/v/restapi/SensorController.java
@@ -24,9 +24,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class SensorController {
 	

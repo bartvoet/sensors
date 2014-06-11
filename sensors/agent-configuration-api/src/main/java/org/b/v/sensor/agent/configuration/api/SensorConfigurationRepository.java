9851ad9e992de33f980dd3dae6268ed2dd7a3083
Bart Voet <bart_voet@telenet.be>
2014-06-11 21:35:23 +0200

    Adding a first implementation for configuration

--- sensors/agent-configuration-api/src/main/java/org/b/v/sensor/agent/configuration/api/SensorConfigurationRepository.java
+++ sensors/agent-configuration-api/src/main/java/org/b/v/sensor/agent/configuration/api/SensorConfigurationRepository.java
@@ -1,7 +1,10 @@
 package org.b.v.sensor.agent.configuration.api;
 
+import java.util.Collection;
+
 import org.b.v.sensors.api.SensorValue;
 
 public interface SensorConfigurationRepository {
 	public void configure(String id,SensorValue value);
+	public Collection<SensorValue> values();
 }
--- sensors/agent-configuration-memory/src/main/java/org/b/v/sensor/agent/configuration/memory/MemorySensorConfigurationRepository.java
+++ sensors/agent-configuration-memory/src/main/java/org/b/v/sensor/agent/configuration/memory/MemorySensorConfigurationRepository.java
@@ -1,14 +1,25 @@
 package org.b.v.sensor.agent.configuration.memory;
 
+import java.util.Collection;
+import java.util.Map;
+
 import org.b.v.sensor.agent.configuration.api.SensorConfigurationRepository;
 import org.b.v.sensors.api.SensorValue;
 
 public class MemorySensorConfigurationRepository implements SensorConfigurationRepository {
 
+	private Map<String,SensorValue> values;
+	
 	@Override
 	public void configure(String sensorId,SensorValue value) {
-		// TODO Auto-generated method stub
-		
+		this.values.put(sensorId, value);
 	}
+
+	@Override
+	public Collection<SensorValue> values() {
+		return values.values();
+	}
+
+	
 	
 }

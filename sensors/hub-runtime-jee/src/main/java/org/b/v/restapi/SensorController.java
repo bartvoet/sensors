184a9a063c1aff8a169fbe1203a50611bdf9426c
Bart Voet <bart_voet@telenet.be>
2014-06-14 18:18:29 +0200

    Adding new rest-methods and dependency to the sensorlib-api

--- sensors/hub-runtime-jee/src/main/java/org/b/v/restapi/SensorController.java
+++ sensors/hub-runtime-jee/src/main/java/org/b/v/restapi/SensorController.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.b.v.sensors.api.SensorValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -70,28 +72,19 @@ public class SensorController {
 			);
 	}
 
-	@RequestMapping(value = "/sensors/{id}/measurements", method = RequestMethod.GET)
+	@RequestMapping(value = "/sensors/{sensorId}/configurations", method = RequestMethod.PUT)
 	@ResponseBody
-	public List<Map<String,Object>> sensorInstructionsOverview(@PathVariable String id) throws IOException, InterruptedException {
-		LOGGER.info("Sensor values");
-		Map<String,Object> parameters = new HashMap<String,Object>();
-		parameters.put("sensorId", id);
-		final DateFormat format = new SimpleDateFormat("yyyy.MM.dd - HH:mm:ss");
-		return jdbcTemplate.query("select * from SensorConfiguration where sensorId=:sensorId and active=1", 
-				parameters, 
-				new RowMapper<Map<String,Object>>(){
-					@Override
-					public Map<String,Object> mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-						Map<String,Object> result = new HashMap<String,Object>();
-						result.put("measurementTime", format.format(rs.getTimestamp("measurementTime")));
-						result.put("functionality", rs.getString("functionality"));
-						result.put("measurementValue", rs.getDouble("measurementValue"));
-						return result;
-					}
-				}
-			);
+	public void applyNewConfiguration(@PathVariable String sensorId,@RequestBody List<SensorValue> configurations) throws IOException, InterruptedException {
+		
 	}
+
+	@RequestMapping(value = "/sensors/{sensorId}/configurations/{configurationId}", method = RequestMethod.GET)
+	@ResponseBody
+	public List<SensorValue> configurationDetail(@PathVariable String sensorId) throws IOException, InterruptedException {
+		return new ArrayList<SensorValue>();
+	}
+	
+
 
 	
 //	create table SensorMeassurement (

100b30135ded6b88a528a2f0a02e75d122c1c366
Bart Voet <bart_voet@telenet.be>
2014-06-15 22:22:15 +0200

    Changing singature of sensor-configuration

--- sensors/agent-configuration-api/src/main/java/org/b/v/sensor/agent/configuration/api/SensorConfigurationRepository.java
+++ sensors/agent-configuration-api/src/main/java/org/b/v/sensor/agent/configuration/api/SensorConfigurationRepository.java
@@ -1,10 +1,11 @@
 package org.b.v.sensor.agent.configuration.api;
 
 import java.util.Collection;
+import java.util.Set;
 
 import org.b.v.values.SensorValue;
 
 public interface SensorConfigurationRepository {
-	public void newConfigurationForSensor(String sensorId,SensorValue value);
+	public void newConfigurationForSensor(String sensorId,Set<SensorValue> value);
 	public Collection<SensorValue> values(String sensorId);
 }
